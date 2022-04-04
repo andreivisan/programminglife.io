7618b7ec1da5e72bf7ae9c8c62fd1dfe8283f4aa
andreivisan <andrei.m.visan@gmail.com>
2022-04-04 09:18:03 +0200

    Updates

---
+++ precache-manifest.4b2e6b150055657c4fec08d5b2bce3ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a323b561a3e8c614c8f2de248ca6bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242caa88e6f5fcbe695b",
+    "url": "/static/css/2.fbf08e0d.chunk.css"
+  },
+  {
+    "revision": "a3285068cbe568fbfdea",
+    "url": "/static/css/main.4d92b7a1.chunk.css"
+  },
+  {
+    "revision": "242caa88e6f5fcbe695b",
+    "url": "/static/js/2.20143d8d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.20143d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3285068cbe568fbfdea",
+    "url": "/static/js/main.9a076142.chunk.js"
+  },
+  {
+    "revision": "7d3853b85d5a894e74ff",
+    "url": "/static/js/runtime-main.0756236b.js"
+  },
+  {
+    "revision": "c64b02ea05a91646997402b21113fe92",
+    "url": "/static/media/me.c64b02ea.jpg"
+  }
+]);
